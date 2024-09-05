f48178323b2c38ead35c65ae645100ce09f7b6be
gwiza-muli <muligand@grinnell.edu>
2024-09-05 09:45:04 -0500

    Update2 HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
   public static void main (String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     pen.println ("Hello, mother!");
+    pen.println("hi there");
     pen.flush();
     pen.close();
   } // main(String[])
